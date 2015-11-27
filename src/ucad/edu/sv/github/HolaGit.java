fa6a902756c0a61f15fc0360e871f1d69cde3b13
kevinmontalvo <leonmontalvokm@hotmail.com>
2015-11-27 01:54:27 -0600

    Hola hoy le puse una linea mas de codigo
    imprime (:"hola GitHub-Y mi nombre":)

--- src/ucad/edu/sv/github/HolaGit.java
+++ src/ucad/edu/sv/github/HolaGit.java
@@ -1,5 +1,9 @@
 package ucad.edu.sv.github;
 
-public class HolaGit {
 
+public class HolaGit {
+	public static void main(String[] args) {
+		
+	System.out.println("hola GitHub-Kevin Montalvo");
+}
 }
